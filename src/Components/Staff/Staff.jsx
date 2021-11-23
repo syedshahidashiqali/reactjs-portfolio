fcbf5e1ba2a52a10fce2d51d627c90def9e2fbd6
Syed Shahid <syedshahid1215@gmail.com>
2021-11-23 17:43:10 +0500

    make the staff component responsive for mobile screens

--- src/Components/Staff/Staff.jsx
+++ src/Components/Staff/Staff.jsx
@@ -5,7 +5,7 @@ function Staff() {
         <div className="staff">
             <div className="container staff-container">
                 <div className="row d-flex ai-c py-5">
-                    <div className="col-md-3 col-sm-12 col-xs-12">
+                    <div className="col-md-3 col-sm-12 col-xs-12 px-0 mb-5 staff-item">
                         <h6 className="staff-title">TEAM & STAFF</h6>
                         <h2 className="staff-team">Our Team</h2>
                         <p className="staff-para">
@@ -14,8 +14,8 @@ function Staff() {
                         </p>
                         <button className="staff-btn">View All Staff</button>
                     </div>
-                    <div className="col-md-3 col-sm-12 col-xs-12 staff-card staff-card-1 px-0">
-                        <img style={{width:"100%", margin:"0px 0px", padding:"0px 0px"}} src="https://preview.colorlib.com/theme/portech/images/xstaff-1.jpg.pagespeed.ic.e9rH06n9-0.webp" alt="" />
+                    <div className="col-md-3 col-sm-12 col-xs-12 staff-card staff-card-1 px-0 mb-5">
+                        <img style={{height:"300px", width:"100%", margin:"0px 0px", padding:"0px 0px"}} src="https://preview.colorlib.com/theme/portech/images/xstaff-1.jpg.pagespeed.ic.e9rH06n9-0.webp" alt="" />
                         <div className="staff-child">
                             <h5>Lloyd Wilson</h5>
                             <h6>WEB DEVELOPER</h6>
@@ -27,8 +27,8 @@ function Staff() {
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-3 col-sm-12 col-xs-12 staff-card staff-card-2 px-0">
-                        <img style={{width:"100%", margin:"0px 0px", padding:"0px 0px"}} src="https://preview.colorlib.com/theme/portech/images/xstaff-2.jpg.pagespeed.ic.336NftRDi2.webp" alt="" />
+                    <div className="col-md-3 col-sm-12 col-xs-12 staff-card staff-card-2 px-0 mb-5">
+                        <img style={{height:"300px", width:"100%", margin:"0px 0px", padding:"0px 0px"}} src="https://preview.colorlib.com/theme/portech/images/xstaff-2.jpg.pagespeed.ic.336NftRDi2.webp" alt="" />
                         <div className="staff-child">
                             <h5>Lloyd Wilson</h5>
                             <h6>WEB DEVELOPER</h6>
@@ -40,8 +40,8 @@ function Staff() {
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-3 col-sm-12 col-xs-12 staff-card staff-card-3 px-0">
-                    <img style={{width:"100%", margin:"0px 0px", padding:"0px 0px"}} src="https://preview.colorlib.com/theme/portech/images/xstaff-3.jpg.pagespeed.ic.uXRWIVCMLJ.webp" alt="" />
+                    <div className="col-md-3 col-sm-12 col-xs-12 staff-card staff-card-3 px-0 mb-5">
+                    <img style={{height:"300px", width:"100%", margin:"0px 0px", padding:"0px 0px"}} src="https://preview.colorlib.com/theme/portech/images/xstaff-3.jpg.pagespeed.ic.uXRWIVCMLJ.webp" alt="" />
                         <div className="staff-child">
                             <h5>Lloyd Wilson</h5>
                             <h6>WEB DEVELOPER</h6>
