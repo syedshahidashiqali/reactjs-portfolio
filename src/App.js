992a1e55675ffba61ba49d7f0550cdd36c1df506
Syed Shahid <syedshahid1215@gmail.com>
2021-11-23 15:13:42 +0500

    add staff components files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Intro from './Components/Intro/Intro';
 import NavBar from './Components/NavBar/NavBar';
+import Staff from './Components/Staff/Staff';
 
 function App() {
   return (
     <div className="app p-r">
       <NavBar />
       <Intro />
+      <Staff />
     </div>
   );
 }
---
+++ src/Components/Staff/Staff.jsx
@@ -0,0 +1,11 @@
+import './Staff.css';
+
+function Staff() {
+    return (
+        <div className="staff">
+            Staff
+        </div>
+    );
+};
+
+export default Staff;
