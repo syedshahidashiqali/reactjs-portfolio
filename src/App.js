1c90ba39884438eed03f3eb2cca4d2037d845994
Syed Shahid <syedshahid1215@gmail.com>
2021-11-26 20:29:38 +0500

    add functionality for changing background image of app every 3 seconds and and add little and smooth transition to that so that changing doesnt look bad and al change is done in App.js and App.css

--- src/App.js
+++ src/App.js
@@ -3,10 +3,27 @@ import Intro from './Components/Intro/Intro';
 import NavBar from './Components/NavBar/NavBar';
 import Staff from './Components/Staff/Staff';
 import Summary from './Components/Summary/Summary';
+import {useState, useEffect} from "react"
 
 function App() {
+  
+  const bgImgArr = [
+    "https://preview.colorlib.com/theme/portech/images/xbg_1.jpg.pagespeed.ic.tSj0cU8YHv.webp",
+    "https://preview.colorlib.com/theme/portech/images/xbg_2.jpg.pagespeed.ic.38jO__TVtA.webp"
+  ];
+  
+  const [bgImg,setBgImg] = useState(bgImgArr[0]);
+
+  setTimeout(() => {
+    if(bgImg == bgImgArr[0]){
+      setBgImg(bgImgArr[1])
+    }else {
+      setBgImg(bgImgArr[0])
+    }
+  }, 3000)
+
   return (
-    <div className="app p-r">
+    <div className="app p-r" style={{backgroundImage: `url(${bgImg})`, transition: "all 1s ease-in"}}>
       <NavBar />
       <Intro />
       <Staff />
